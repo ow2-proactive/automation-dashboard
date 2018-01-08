7755bf611aeca4af0717ca256b310c5b2555e759
chloegugli <chloe.guglielmi@activeeon.com>
2018-01-08 12:23:53 +0100

    Adding the possibility to have extra html files + renaming files

--- app/templates_versions/community/app.js
+++ app/templates_versions/community/app.js
@@ -7,7 +7,6 @@
         'ui.router', // Routing
         'ui.bootstrap', // Bootstrap
         'main',
-        'pca-rest',
         //!DO NOT EDIT! The following code related to subviews is automatically generated with grunt build. You can't modify it from here.
         //See 'replace' task in Gruntfile.js and subviews definition in enterpriseSubviews.json.
 
