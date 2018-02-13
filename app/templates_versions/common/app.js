187034b50310c6f060223d6a919c7ab4e4f8c4c2
chloegugli <chloe.guglielmi@activeeon.com>
2018-02-13 18:19:47 +0100

    job planner portal : changing time format

--- app/templates_versions/common/app.js
+++ app/templates_versions/common/app.js
@@ -4,17 +4,22 @@
  */
 (function() {
     angular.module('inspinia', [
-        'ui.router', // Routing
-        'ui.bootstrap', // Bootstrap
-        'main',
-        'angular-cron-gen',
-        'moment-picker',
-        //!DO NOT EDIT! The following code related to subviews is automatically generated with grunt build. You can't modify it from here.
-        //See 'replace' task in Gruntfile.js and subviews definition in enterpriseSubviews.json.
+            'ui.router', // Routing
+            'ui.bootstrap', // Bootstrap
+            'main',
+            'angular-cron-gen',
+            'moment-picker',
+            //!DO NOT EDIT! The following code related to subviews is automatically generated with grunt build. You can't modify it from here.
+            //See 'replace' task in Gruntfile.js and subviews definition in enterpriseSubviews.json.
 
-        //beginSubviewsModules
+            //beginSubviewsModules
 
-        //endSubviewsModules
-        'angularCSS'
-    ])
+            //endSubviewsModules
+            'angularCSS'
+        ])
+        .config(['momentPickerProvider', function(momentPickerProvider) {
+            momentPickerProvider.options({
+                minutesFormat: 'HH:mm'
+            });
+        }])
 })();
