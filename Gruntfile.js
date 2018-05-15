a54fb30918aa8abd00d933831dfb2cdeafe826b8
chloegugli <chloe.guglielmi@activeeon.com>
2018-05-15 17:03:26 +0200

    Writing eslint file that makes code compile more easily

--- Gruntfile.js
+++ Gruntfile.js
@@ -404,6 +404,26 @@ module.exports = function (grunt) {
         },
         usemin: {
             html: ['dist/index.html']
+        },
+        eslint : {
+            options: {
+                configFile : ".eslintrc.json"
+            },
+            target : "cloud-automation/app/scripts/cloud-automation-rest.js"
+                /*(function () {
+                var includedScripts = [];
+                for (var key in subviewsDefinition) {
+                    if (subviewsDefinition[key].isAvailable && !subviewsDefinition[key].isSubMenuTitle) {
+                        for (var scriptKey in subviewsDefinition[key].jsFiles) {
+                            var script = subviewsDefinition[key].jsFiles[scriptKey];
+                            if (includedScripts.indexOf(script) < 0) {
+                                includedScripts.push(subviewsDefinition[key].appFolder+script);
+                            }
+                        }
+                    }
+                }
+                return includedScripts;
+            })()*/
         }
     });
 
@@ -415,6 +435,7 @@ module.exports = function (grunt) {
 
     // building of the common parts of a build
     grunt.registerTask('pre-build', [
+        'eslint',
         'clean:dist',
         'replace',
         'jsbeautifier',
