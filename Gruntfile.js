2a3ca4b7682890bc1e7c1e1bcf324cb304c9fb18
Mohamed BOUSSAA <mohamed.boussaa@activeeon.com>
2020-02-04 12:09:44 +0100

    Fix AD (#347)

--- Gruntfile.js
+++ Gruntfile.js
@@ -420,10 +420,9 @@ module.exports = function (grunt) {
         jsbeautifier: {
             files: [
                 '<%= inspinia.app %>/scripts/config.js',
-                '<%= inspinia.app %>/views/common/navigation.html',
                 '<%= inspinia.app %>/scripts/app.js',
-                '<%= inspinia.app %>/index.html',
-                '<%= inspinia.app %>/<%= inspinia.conf %>/{,*/}*.*'
+                '<%= inspinia.app %>/<%= inspinia.conf %>/{,*/}*.*',
+                '!<%= inspinia.app %>/{,*/}{,*/}*.html'
             ],
             options: {
             }
@@ -449,7 +448,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= inspinia.dist %>',
-                    src: ['*.html', 'views/{,*/}*.html', '!index.html', '!views/common/navigation.html'],
+                    src: ['*.html', 'views/{,*/}*.html'],
                     dest: '<%= inspinia.dist %>'
                 }]
             }
--- app/templates_versions/common/app.js
+++ app/templates_versions/common/app.js
@@ -4,35 +4,39 @@
  */
 (function() {
     angular.module('inspinia', [
-            'ui.router', // Routing
-            'ui.bootstrap', // Bootstrap
-            'main',
-            'angularMoment',
-            'angular-cron-gen',
-            'moment-picker',
-            'mwl.calendar',
-            //!DO NOT EDIT! The following code related to subviews is automatically generated with grunt build. You can't modify it from here.
-            //See 'replace' task in Gruntfile.js and subviews definition in enterpriseSubviews.json.
+        'ui.router', // Routing
+        'ui.bootstrap', // Bootstrap
+        'main',
+        'angularMoment',
+        'angular-cron-gen',
+        'moment-picker',
+        'mwl.calendar',
+        //!DO NOT EDIT! The following code related to subviews is automatically generated with grunt build. You can't modify it from here.
+        //See 'replace' task in Gruntfile.js and subviews definition in enterpriseSubviews.json.
 
-            //beginSubviewsModules
+        //beginSubviewsModules
 
-            //endSubviewsModules
-            'angularCSS',
-            'ui.codemirror',
-            'gantt',
-            'gantt.tooltips',
-            'ui.tree',
-            'gantt.tree',
-            'gantt.groups',
-            'gantt.corner'
+        //endSubviewsModules
+        'angularCSS',
+        'ui.codemirror',
+        'gantt',
+        'gantt.tooltips',
+        'ui.tree',
+        'gantt.tree',
+        'gantt.groups',
+        'gantt.corner'
+    ])
+        .config(['momentPickerProvider',
+            function(momentPickerProvider) {
+                momentPickerProvider.options({
+                    minutesFormat: 'HH:mm'
+                });
+            }
         ])
-        .config(['momentPickerProvider', function(momentPickerProvider) {
-            momentPickerProvider.options({
-                minutesFormat: 'HH:mm'
-            });
-        }])
-        .config(['calendarConfig', function(calendarConfig) {
-            calendarConfig.allDateFormats.moment.date.hour = 'HH:mm';
-            calendarConfig.showTimesOnWeekView = true;
-        }])
+        .config(['calendarConfig',
+            function(calendarConfig) {
+                calendarConfig.allDateFormats.moment.date.hour = 'HH:mm';
+                calendarConfig.showTimesOnWeekView = true;
+            }
+        ])
 })();
