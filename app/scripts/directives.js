04e5c680eb3de745f3f3ee9e964cf22db7867e15
Amine Mouhoub <ma.mouhoub@gmail.com>
2020-04-08 09:01:36 +0200

    Add back-to-top directive and new dependency for infinite-scroll

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -208,6 +208,81 @@ function slimScroll($timeout) {
     };
 }
 
+
+/**
+ * backToTop - from https://github.com/padsbanger/angular-backtop
+ * with slight modifications to remove themes
+ */
+function backToTop() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        template: '<div id="backtop"><button><div ng-transclude></div></button></div>',
+        scope: {
+            text: "@buttonText",
+            speed: "@scrollSpeed"
+        },
+        link: function(scope, element) {
+
+            scope.text = scope.text || 'Scroll top';
+            scope.speed = parseInt(scope.speed, 10) || 300;
+
+            var speed = Math.round(scope.speed / 100);
+            var onscroll = function() {
+                if (window.pageYOffset > 0) {
+                    if(!element.showing){
+                        element.addClass('show');
+                        element.showing = true;
+                    }
+                } else {
+                    element.removeClass('show');
+                    element.showing = false;
+                }
+            };
+
+            scope.currentYPosition = function() {
+                if (document.documentElement && document.documentElement.scrollTop)
+                    return document.documentElement.scrollTop;
+                if (document.body.scrollTop)
+                    return document.body.scrollTop;
+                return 0;
+            };
+
+            element.showing = false;
+            element.on('click', function() {
+                window.removeEventListener("scroll", onscroll);
+
+                element.removeClass('show');
+                element.showing = false;
+
+                var startY = scope.currentYPosition();
+
+                var step = Math.round(startY / 25);
+                var leapY = startY < 100 ? 0 : startY - step;
+
+                var scrollInterval = setInterval(function(){
+                    window.scrollTo(0, leapY)
+                    if(!leapY){
+                        clearInterval(scrollInterval);
+                        window.addEventListener("scroll", onscroll)
+                    }
+
+                    leapY -= step;
+
+                    if(leapY < 0) leapY = 0;
+                }, speed)
+            });
+
+            window.addEventListener("scroll", onscroll);
+            scope.$on("$destroy", function(){
+                window.removeEventListener("scroll", onscroll)
+            })
+        }
+    };
+
+}
+
 /**
  *
  * Pass all functions into module
@@ -220,4 +295,5 @@ angular
     .directive('minimalizaSidebar', minimalizaSidebar)
     .directive('pageTitle', pageTitle)
     .directive('sideNavigation', sideNavigation)
-    .directive('slimScroll', slimScroll);
+    .directive('slimScroll', slimScroll)
+    .directive('backTop', backToTop);
