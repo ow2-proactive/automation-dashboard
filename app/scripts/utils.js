974aa9d718632db59d583b520dfdca0597573ee9
TAO Xinxiu (Isabelle) <isabelle.tao@activeeon.com>
2020-06-12 14:46:49 +0200

    fix: add utils func to check if a variable needs special UI (#387)

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -1,4 +1,6 @@
 function UtilsFactory($window) {
+    var specialUIModel = ['pa:boolean', 'pa:list', 'pa:datetime', 'pa:hidden', 'pa:global_file', 'pa:user_file', 'pa:optional_global_file', 'pa:optional_user_file', 'pa:credential'];
+
     function openJobInSchedulerPortal(jobId) {
         if (jobId) {
             var url = JSON.parse(localStorage.schedulerPortalUrl)+ '/?job=' + jobId;
@@ -6,6 +8,7 @@ function UtilsFactory($window) {
             win.focus();
         }
     }
+
     function updateCursor(isWaiting) {
         var body = angular.element(document).find('body');
         if (isWaiting) {
@@ -13,10 +16,19 @@ function UtilsFactory($window) {
         } else {
             body.removeClass('waiting');
         }
+    }
+
+    // check whether the variable needs a special UI (i.e., it's not simply showing its value with an inputbox)
+    // For example, if the variable is the type 'pa:boolean', 'pa:list', or 'pa:datetime' etc, the function return true
+    function isSpecialUIModel(variable) {
+        return -1 !== specialUIModel.findIndex(function (targetModel) {
+            return variable.model.toLowerCase().indexOf(targetModel) !== -1;
+        });
     };
 
     return {
         openJobInSchedulerPortal : openJobInSchedulerPortal,
+        isSpecialUIModel: isSpecialUIModel,
         updateCursor : function(isWaiting){
             return updateCursor(isWaiting);
         }
