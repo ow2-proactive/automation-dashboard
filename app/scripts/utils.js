1d3fcf70b7d1fb079853614a85bcc908a2a951cc
tao-xinxiu <isabelle.tao@orange.com>
2020-06-12 13:49:24 +0200

    fix: add func to check whether a variable's model needs special UI during workflow submission.

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -6,6 +6,7 @@ function UtilsFactory($window) {
             win.focus();
         }
     }
+
     function updateCursor(isWaiting) {
         var body = angular.element(document).find('body');
         if (isWaiting) {
@@ -13,10 +14,22 @@ function UtilsFactory($window) {
         } else {
             body.removeClass('waiting');
         }
+    }
+
+    // check whether the variable needs a special UI (i.e., it's not simply showing its value with an inputbox)
+    // For example, if the variable is the type 'pa:boolean', 'pa:list', or 'pa:datetime' etc, the function return true
+    var specialUIModel = ['pa:boolean', 'pa:list', 'pa:datetime', 'pa:hidden', 'pa:global_file', 'pa:user_file', 'pa:optional_global_file', 'pa:optional_user_file', 'pa:credential'];
+    function isSpecialUIModel(variable) {
+        function matchTargetModel(targetModel) {
+            return variable.model.toLowerCase().indexOf(targetModel) != -1;
+        }
+        var index = specialUIModel.findIndex(matchTargetModel);
+        return index != -1;
     };
 
     return {
         openJobInSchedulerPortal : openJobInSchedulerPortal,
+        isSpecialUIModel: isSpecialUIModel,
         updateCursor : function(isWaiting){
             return updateCursor(isWaiting);
         }
