99a9101a087d63f07b1ed279696aba189195715f
TAO Xinxiu (Isabelle) <isabelle.tao@activeeon.com>
2022-04-07 23:35:16 +0200

    fix the bug of uploading files in the debug info disabled environment. (#500)

--- app/templates_versions/common/app.js
+++ app/templates_versions/common/app.js
@@ -2,7 +2,7 @@
  * INSPINIA - Responsive Admin Theme
  *
  */
-(function () {
+(function() {
     angular.module('inspinia', [
         'ui.router', // Routing
         'ui.bootstrap', // Bootstrap
@@ -27,20 +27,20 @@
         'gantt.corner'
     ])
         .config(['momentPickerProvider',
-            function (momentPickerProvider) {
+            function(momentPickerProvider) {
                 momentPickerProvider.options({
                     minutesFormat: 'HH:mm'
                 });
             }
         ])
         .config(['calendarConfig',
-            function (calendarConfig) {
+            function(calendarConfig) {
                 calendarConfig.allDateFormats.moment.date.hour = 'HH:mm';
                 calendarConfig.showTimesOnWeekView = true;
             }
         ])
         .config(['$compileProvider',
-            function ($compileProvider) {
+            function($compileProvider) {
                 $compileProvider.debugInfoEnabled(false);
             }
         ]);
