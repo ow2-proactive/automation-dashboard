7f2a90d48f5a040f63050587f1e298a2e221c4b2
oanaBiancaSchiopu <80052076+oanaBiancaSchiopu@users.noreply.github.com>
2023-03-09 19:20:54 +0200

    Set submission mode (#560)
    * set-submission-mode
    
    * set-submission-mode
    
    ---------
    
    Co-authored-by: Oana Schiopu <oana-schiopu@activeeon.com>

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -599,7 +599,7 @@ function UtilsFactory($window, $uibModal, $filter, $cookies, $http, $rootScope, 
         return $http.post(schedulerRestUrl() + 'validateurl/body', data, configHeaders);
     }
 
-    function submitJob(bucketName, workflowName, variables) {
+    function submitJob(bucketName, workflowName, variables, submissionMode) {
         const configHeaders = {
             headers: {
                 'link': catalogUrlPrefix + bucketName + '/resources/' + encodeURIComponent(workflowName) + '/raw',
@@ -612,7 +612,7 @@ function UtilsFactory($window, $uibModal, $filter, $cookies, $http, $rootScope, 
             return map;
         }, {});
         var data = JSON.stringify(variablesMap);
-        return $http.post(schedulerRestUrl() + 'jobs/body', data, configHeaders);
+        return $http.post(schedulerRestUrl() + 'jobs/body?submission.mode='+ submissionMode, data, configHeaders);
     }
 
     function getJobInfoForJob(jobId) {
