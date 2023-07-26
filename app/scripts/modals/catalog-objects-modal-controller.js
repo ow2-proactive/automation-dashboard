caf166640b2652983ae9c9caebfc603048f50551
Mohamed Khalil LABIDI <47785603+mklkun@users.noreply.github.com>
2023-07-26 09:37:25 +0200

    Add object name filtering in Catalog Objects Modal (#581)
    * Add object name filtering in Catalog Objects Modal
    
    * Apply Fabien's comments
    
    * Fix build error
    
    ---------
    
    Co-authored-by: mklkun <khalil.labidi@activeeon.com>

--- app/scripts/modals/catalog-objects-modal-controller.js
+++ app/scripts/modals/catalog-objects-modal-controller.js
@@ -5,10 +5,15 @@ angular.module('workflow-variables').controller('CatalogObjectsModalCtrl', funct
         var params = matches[1].split(',');
         var kindFilter = params[0];
         var filterContentType = params[1];
+        var objectNameFilter = params[3];
     }
-    var kindFilterUrl = (kindFilter) ? 'kind=' + kindFilter : '';
-    var contentFilterUrl = (filterContentType) ? 'contentType=' + filterContentType : '';
-    var filterUrlParams = [kindFilterUrl, contentFilterUrl].join('&');
+
+    var kindFilterUrl = (kindFilter) ? 'kind=' + encodeURIComponent(kindFilter) : '';
+    var contentFilterUrl = (filterContentType) ? 'contentType=' + encodeURIComponent(filterContentType) : '';
+    var objectNameFilterUrl = (objectNameFilter) ? "objectName=" + encodeURIComponent(objectNameFilter) : "";
+    var filterUrlParams = [kindFilterUrl, contentFilterUrl, objectNameFilterUrl].filter(function(x) {
+        return (typeof x === 'string' && x.length > 0)
+    }).join('&');
 
     $scope.updateBuckets = function() {
         $scope.clearData();
