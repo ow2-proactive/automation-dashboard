804e281c10919e7d2bfe35d0bf244d01f2331384
Mohamed Boushab <medou.boushab@activeeon.com>
2023-09-20 13:13:16 +0200

    Fix: Add or update a new association to a calendar always select the first calendar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1171,7 +1171,7 @@ angular.module('main').controller('VariablesController', function ($scope, $uibM
                 // create association
                 $scope.$parent.createAssociation($scope.workflow.name, bucketName, $scope.workflow.variables)
                     .success(function (res) {
-                        $scope.updatePlannedJobsListAndSelect(res.id);
+                        //$scope.updatePlannedJobsListAndSelect(res.id);
                         $rootScope.$broadcast('event:updatePlannedJobsCount');
                         $scope.desectWorkflowInModal();
                         $scope.toggleOpenSubmitJobPanel(false);
@@ -1250,7 +1250,6 @@ angular.module('main').controller('VariablesController', function ($scope, $uibM
                 // Check Successful - proceed to edit
                 $scope.$parent.updateCdWfAssociation($scope.workflow.variables)
                     .success(function (res) {
-                        $scope.updatePlannedJobsListAndSelect(res.id);
                         displaySuccessMessage('Association successfully updated');
                         $scope.updateLastSelectedWorkflow();
                         $scope.isSubmissionGoingOn = false;
