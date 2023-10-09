e84cab3b71880af91d296c6dc69407145bb57539
oanaBiancaSchiopu <80052076+oanaBiancaSchiopu@users.noreply.github.com>
2023-10-09 16:14:22 +0200

    Login automatically when user is logges from another portal (#605)
    * login-automatically-when-user-is-logges-from-another-portal
    
    * login-automatically-when-user-is-logges-from-another-portal
    
    * set-user
    
    ---------
    
    Co-authored-by: Oana Schiopu <oana-schiopu@activeeon.com>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -247,6 +247,12 @@ mainModule.controller('mainController', function ($window, $http, $scope, $rootS
                         $scope.closeSession();
                     } else {
                         $rootScope.isLoggedOut = false;
+                        $scope.main.userName = localStorage['pa.login'];
+                        var sessionid = getSessionId();
+                        if (sessionid) {
+                            $scope.determineFirstAuthorizedPortalAndAllPortalsAccessPermission($scope.redirectsTo);
+                        }
+                        $rootScope.serverIsDown = false;
                     }
                 })
                 .catch(function (response) {
@@ -354,7 +360,6 @@ mainModule.controller('mainController', function ($window, $http, $scope, $rootS
         }
 
         localStorage.removeItem('pa.session');
-        $scope.stopRegularCheckSession();
         $rootScope.$broadcast('event:StopRefreshing');
     };
 
